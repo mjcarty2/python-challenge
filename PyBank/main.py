cf2bab333d46de3ddb8e2da9a1acba2ae545ba61
Matthew Carty <mjcarty2@gmail.com>
2018-09-20 18:51:19 -0400

    final commit

---
+++ PyBank/main.py
@@ -0,0 +1,41 @@
+import csv
+import os
+
+file = "budget_data.csv"
+
+list_months = []
+total_revenue = 0
+revenue_list = []
+monthly_changes = []
+monthly_increases = []
+total_monthly_rev_change = 0
+
+with open(file, "r") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for (row) in reader:
+        current_month = row["Date"]
+        current_revenue = int(row["Profit/Losses"])
+        list_months.append(current_month)
+        revenue_list.append(current_revenue)
+        total_revenue = total_revenue + current_revenue
+
+for i in range(0, len(revenue_list)-1):
+    month_change = revenue_list[i+1] - revenue_list[i]
+    monthly_changes.append(month_change)
+    total_monthly_rev_change= total_monthly_rev_change + month_change
+    
+print("Financial Analysis")
+print("------------------")
+print(f'Total Months: {len(list_months)}')
+print(f"Net Profit/Loss: {total_revenue}")
+print(f'Average Profit/Loss Change: {total_monthly_rev_change / len(monthly_changes)}')
+print(f'Greatest Profit:  {list_months[monthly_changes.index(max(monthly_changes)) + 1]} {max(monthly_changes)}')
+print(f'Greatest Loss: {list_months[monthly_changes.index(min(monthly_changes)) + 1]}  {min(monthly_changes)}')
+
+budget_data = open("Results.txt", "w")
+budget_data.write("Financial Analysis \n") 
+budget_data.write(f'Total Months: {len(list_months)} \n')
+budget_data.write(f'Net Profit/Loss: {total_revenue} \n')
+budget_data.write(f'Average Profit/Loss Change: {total_monthly_rev_change / len(monthly_changes)} \n')
+budget_data.write(f'Greatest Profit:  {list_months[monthly_changes.index(max(monthly_changes)) + 1]} {max(monthly_changes)} \n')
+budget_data.write(f'Greatest Loss: {list_months[monthly_changes.index(min(monthly_changes)) + 1]}  {min(monthly_changes)}')
---
+++ PyPoll/main.py
@@ -0,0 +1,35 @@
+import csv
+import os
+
+file = "election_data.csv"
+
+total_votes = 0
+candidate = []
+votes_count = []
+
+with open(file, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+   
+    for row in reader:
+        total_votes = total_votes+1
+        if row[1] in candidate:
+            vote = candidate.index(row[2])
+            votes_count[vote]+=1
+        else:
+            candidate.append(str(row[2]))
+            votes_count.append(1)
+
+print("Election Results")
+print("Total Votes: "+str(total_votes))
+for x in range(0, len(candidate)):
+        print(candidate[x]+": "+str(round((100*votes_count[x]/total_votes),2))+"% "+str(votes_count[x]))
+winner = votes_count.index(max(votes_count))
+print("Winner: "+candidate[winner])
+
+
+election_results_file = open("Results.txt", "w")
+election_results_file.write("Elections Results\n")
+election_results_file.write(f'Total Votes: {total_votes}\n')
+for x in range(0, len(candidate)):
+        election_results_file.writerow([candidate[x], str(round((100*votes_count[x]/total_votes),2)), str(votes_count[x])])
+election_results_file.write(f'Winner: {winner}')
